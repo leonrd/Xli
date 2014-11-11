eea5f5b449575c09360e10a40fe18cbeb6195e60
Baggers <chris.bagley@gmail.com>
2014-11-11 14:28:43 +0100

    Fixes for ios audio

--- src/XliSoundPlayer/coreaudio/CoreSoundPlayer.mm
+++ src/XliSoundPlayer/coreaudio/CoreSoundPlayer.mm
@@ -2,140 +2,168 @@
 #import <AVFoundation/AVFoundation.h>
 #include <XliPlatform/PlatformSpecific/iOS.h>
 #include <XliSoundPlayer/SoundPlayer.h>
+#include "CoreSoundDelegate.h"
 #include <Xli/Array.h>
 #include <Xli/Console.h>
 #include <assert.h>
 
+@implementation AudioDelegate
+
+- (id) initWithCPPInstance:(Xli::CoreSoundChannel*)cppInstance
+{
+    self = [super init];
+    if (self) {
+        m_handlerInstance = cppInstance;
+    }
+    return self;
+}
+
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag
+{
+    [player stop];
+    [player prepareToPlay];
+    player.currentTime = 0;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Dont really want to clean up here as should probably be higher system task
+}
+
+@end
+
 namespace Xli
-{      
-
-    class CoreSoundChannel : public SoundChannel
+{       
+    CoreSoundChannel::CoreSoundChannel(const char* filename, bool loop, bool play)
     {
-        AVAudioPlayer* player;
-            
-    public:
-        CoreSoundChannel(const char* filename, bool loop, bool play)
-        {
-            NSString* pathString = [NSString stringWithFormat:@"%@/data/%@", [[NSBundle mainBundle] resourcePath], [NSString stringWithUTF8String:filename]];
+        NSString* pathString = [NSString stringWithFormat:@"%@/data/%@", [[NSBundle mainBundle] resourcePath], [NSString stringWithUTF8String:filename]];
                 
-            NSURL* url = [NSURL fileURLWithPath:pathString];
-            //[pathString release];
+        NSURL* url = [NSURL fileURLWithPath:pathString];
+        //[pathString release];
                 
-            NSError* error;
+        NSError* error;
             
-            BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:pathString];
-            if (!fileExists) {
-                const char* wrongPath = [pathString UTF8String];
-                Error->WriteLine("SoundPlayer: Audio file not found at:");
-                Error->WriteLine(wrongPath);
-                player = nil;
-                return;
-            }
-            
-            player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
-            //player = [[AVAudioPlayer alloc] initWithData:[NSData dataWithContentsOfURL:url] error:&error];
-            //[url release];
-            
-            if (player == nil)
-            {
-                Error->WriteLine("ERROR: Failed to create AVAudioPlayer: " + (String)([[error description] UTF8String]));
-                return;
-            }
-                
-            if (loop)
-            {
-                player.numberOfLoops = -1;
-            }
-                
-            if (play)
-            {
-                [player play];
-            }
+        BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:pathString];
+        if (!fileExists) {
+            const char* wrongPath = [pathString UTF8String];
+            Error->WriteLine("SoundPlayer: Audio file not found at:");
+            Error->WriteLine(wrongPath);
+            player = nil;
+            return;
         }
             
-        virtual ~CoreSoundChannel()
+        player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+        //player = [[AVAudioPlayer alloc] initWithData:[NSData dataWithContentsOfURL:url] error:&error];
+        //[url release];
+            
+        if (player == nil)
         {
-            if (player == nil) return;
-            [player release];
-        }
-        
-        virtual void Pause()
-        {
-            if (player == nil) return;
-            [player pause];
-        }
-        virtual void UnPause()
-        {
-            if (player == nil) return;
-            [player play];
-        }
-        
-        virtual double GetPosition() const
-        {
-            if (player == nil) return 0.0;
-            return player.currentTime;
-        }
-        virtual void SetPosition(double value)
-        {
-            if (player == nil) return;
-            player.currentTime = value;
+            Error->WriteLine("ERROR: Failed to create AVAudioPlayer: " + (String)([[error description] UTF8String]));
+            return;
         }
 
-        virtual float GetVolume() const
-        {
-            if (player == nil) return 0.0f;
-            return player.volume;
-        }
-        virtual void SetVolume(float value) const
-        {
-            if (player == nil) return;
-            player.volume = value;
-        }
-        virtual bool IsPlaying()
-        {
-            if (player == nil) return false;
-            return player.playing;
-        }
-        virtual bool IsFinished()
-        {
-            if (player == nil) return true;
-            return false;
-        }
-   
-        double GetDuration() const
-        {
-            if (player == nil || player == 0) return 0;
-            return (double)player.duration;
-        }
-        virtual float GetPan() const
-        {
-            if (player == nil) return 0.0f;
-            return player.pan;
-        }
-        virtual void SetPan(float value) const
-        {
-            if (player == nil) return;
-            player.pan = value;
-        }
+        audioDelegate = [[AudioDelegate alloc] initWithCPPInstance:this];
+        player.delegate = audioDelegate;
         
-        virtual void Play()
+        if (loop)
         {
-            if (player == nil) return;
+            player.numberOfLoops = -1;
+        }
+
+        [player prepareToPlay];
+        if (play)
+        {
             [player play];
         }
-        virtual void Stop()
-        {
-            if (player == nil) return;
-            [player stop];
-        }
+    }
+            
+    CoreSoundChannel::~CoreSoundChannel()
+    {
+        if (player == nil) return;
+        [player release];
+    }
         
-        virtual bool IsPaused()
-        {
-            if (player == nil) return true;
-            return !player.playing;
-        }
-    };
+    void CoreSoundChannel::Pause()
+    {
+        if (player == nil) return;
+        [player pause];
+    }
+    void CoreSoundChannel::UnPause()
+    {
+        if (player == nil) return;
+        [player play];
+    }
         
+    double CoreSoundChannel::GetPosition() const
+    {
+        if (player == nil) return 0.0;
+        return player.currentTime;
+    }
+    void CoreSoundChannel::SetPosition(double value)
+    {
+        if (player == nil) return;
+        player.currentTime = value;
+    }
+
+    float CoreSoundChannel::GetVolume() const
+    {
+        if (player == nil) return 0.0f;
+        return player.volume;
+    }
+    void CoreSoundChannel::SetVolume(float value) const
+    {
+        if (player == nil) return;
+        player.volume = value;
+    }
+    bool CoreSoundChannel::IsPlaying()
+    {
+        if (player == nil) return false;
+        return player.playing;
+    }
+    bool CoreSoundChannel::IsFinished()
+    {
+        if (player == nil) return true;
+        return false;
+    }
+   
+    double CoreSoundChannel::GetDuration() const
+    {
+        if (player == nil || player == 0) return 0;
+        return (double)player.duration;
+    }
+    float CoreSoundChannel::GetPan() const
+    {
+        if (player == nil) return 0.0f;
+        return player.pan;
+    }
+    void CoreSoundChannel::SetPan(float value) const
+    {
+        if (player == nil) return;
+        player.pan = value;
+    }
+        
+    void CoreSoundChannel::Play()
+    {
+        if (player == nil) return;
+        [player play];
+    }
+    void CoreSoundChannel::Stop()
+    {
+        if (player == nil) return;
+        [player stop];
+        player.currentTime = 0;
+        [player prepareToPlay];
+    }
+        
+    bool CoreSoundChannel::IsPaused()
+    {
+        if (player == nil) return true;
+        return !player.playing;
+    }
+
+
+
+    
     class CoreSound : public Sound 
     {
     private:
