357a2816e81fed360d67be34d58af03e8569a458
morten <morten@outracks.no>
2015-02-09 14:35:05 +0100

    make pack-android.sh work on windows

--- pack-android.sh
+++ pack-android.sh
@@ -8,5 +8,10 @@ VERSION="$DATE-${SHA:0:6}"
 BASENAME="Xli-$VERSION-Android"
 
 rm -rf lib
-sh build-android.sh
-zip -r "$BASENAME.zip" include lib
+if [ "$OSTYPE" = "msys" ]; then
+	cmd //c build-android.bat
+	"C:\Program Files\7-Zip\7z.exe" a -y "$BASENAME.zip" include lib
+else
+	sh build-android.sh
+	zip -r "$BASENAME.zip" include lib
+fi
